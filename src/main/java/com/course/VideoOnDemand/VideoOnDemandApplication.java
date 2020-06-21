a060e9891d98cf52f427d46f8bfe0e9fc690cb08
YyJavaWorkPlace <13383448815@163.com>
2020-06-21 11:52:57 +0800

    spring 初始化项目

---
+++ src/main/java/com/course/VideoOnDemand/VideoOnDemandApplication.java
@@ -0,0 +1,13 @@
+package com.course.VideoOnDemand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoOnDemandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoOnDemandApplication.class, args);
+	}
+
+}
