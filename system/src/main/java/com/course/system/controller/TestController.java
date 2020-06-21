057239afaab9647eaa789b3fe24c11a19707f1ed
YyJavaWorkPlace <13383448815@163.com>
2020-06-21 22:49:31 +0800

    1.mybatis 使用示例 http://127.0.0.1:9001/system/test 2.使用database关联数据库 3.新增数据库脚本 4.集成热部署DevTools （引入依赖，搜索Compiler 勾选Build Project automatically 连按2次shift 搜索registry勾选compiler.automake.allow.when.app.running 5.优化mybatis日志输出

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -1,15 +1,25 @@
 package com.course.system.controller;
 
+import com.course.system.domain.Test;
+import com.course.system.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.xml.ws.Action;
+import java.util.List;
 
 /**
  * 接口
  */
 @RestController
 public class TestController {
-    @RequestMapping("/test")
-    public String test(){
-        return "Success";
+    @Autowired
+    private TestService testService;
+    @RequestMapping(value="/test",produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Test> test()
+    {
+        return testService.list();
     }
 }
