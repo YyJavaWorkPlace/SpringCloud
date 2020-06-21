191c2ee9290a4fdfb58f1f4b905501c80848f144
YyJavaWorkPlace <13383448815@163.com>
2020-06-21 15:24:20 +0800

    新增gateway子模块，增加gateway依赖，并注册到注册中心

--- gateway/src/main/java/gateway/GatewayApplication.java
+++ gateway/src/main/java/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.course.system;
+package gateway;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,14 +12,16 @@ import org.springframework.core.env.Environment;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class SystemApplication {
-    private static final Logger LOG = LoggerFactory.getLogger(SystemApplication.class);
+public class GatewayApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(GatewayApplication.class);
 
+    //	public static void main(String[] args) {
+//		SpringApplication.run(EurekaApplication.class, args);
+//	}
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(SystemApplication.class);
+        SpringApplication app = new SpringApplication(GatewayApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功");
-        LOG.info("system地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+        LOG.info("Gateway地址：\thttp://127.0.0.1:{}", env.getProperty("server.port"));
     }
-
 }
