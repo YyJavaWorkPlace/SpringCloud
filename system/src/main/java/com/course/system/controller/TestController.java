2e6ac987b33fe1225842303972d731f0363c08a7
YyJavaWorkPlace <13383448815@163.com>
2020-06-22 23:20:02 +0800

    Example使用示例 使用Example 可以快速完成单表的增删改查 熟练掌握后对代码开发效率将有一个质的飞跃

--- system/src/main/java/com/course/system/controller/TestController.java
+++ system/src/main/java/com/course/system/controller/TestController.java
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 
 import java.util.List;
 
