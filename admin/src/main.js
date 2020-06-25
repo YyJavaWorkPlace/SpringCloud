d8e57986373f8c79273c7a77bd144721b91f339c
YyJavaWorkPlace <13383448815@163.com>
2020-06-25 11:47:22 +0800

    增加login页面 增加路由配置 访问/login时 加载login组件

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
+import router from './router'
+Vue.config.productionTip = false;
 // vue入口文件，用来初始化vue实例并集成所需要的插件
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
