f900ace6c6eb8a761ddcf5480fbeedc322dcfb0f
YyJavaWorkPlace <13383448815@163.com>
2020-06-26 15:25:09 +0800

    1.集成分页插件 注意页码从1开始

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -5,6 +5,7 @@ import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -23,6 +24,8 @@ public class ChapterService {
      * @return
      */
     public List<ChapterDto> list() {
+        //遇到的第一个select语句会进行分页
+        PageHelper.startPage(1,1);
         ChapterExample chapterExample = new ChapterExample();
         List<Chapter> chapterList = chapterMapper.selectByExample(chapterExample);
         return Chapter2ChapterDTOConverter.convert(chapterList);
