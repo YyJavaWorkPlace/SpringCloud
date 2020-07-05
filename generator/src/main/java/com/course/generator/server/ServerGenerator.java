245357c0685131920cb90b5e97891fd05e5a90f7
YyJavaWorkPlace <13383448815@163.com>
2020-07-05 10:20:46 +0800

    生成器开发:与mybatis-generator 整合

--- generator/src/main/java/com/course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -4,7 +4,13 @@ import com.course.generator.util.DbUtil;
 import com.course.generator.util.Field;
 import com.course.generator.util.FreemarkUtil;
 import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import javax.xml.transform.sax.SAXResult;
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.*;
@@ -14,20 +20,37 @@ public class ServerGenerator {
     static String toServicePath = "server\\src\\main\\java\\com\\course\\server\\service\\";
     static String toDtoPath = "server\\src\\main\\java\\com\\course\\server\\dto\\";
     static String toControllerPath = MODULE + "\\src\\main\\java\\com\\course\\business\\controller\\admin\\";
+    static String generatorConfigPath = "server\\src\\main\\resources\\generator\\generatorConfig.xml";
 
-    public static void main(String[] args) throws IOException, TemplateException, SQLException {
-        String Domain = "Section";
-        String domain = "section";
-        String tableNameCn = "小节";
+    public static void main(String[] args) throws IOException, TemplateException, SQLException, DocumentException {
+        // 读取XML文件
+        File file = new File(generatorConfigPath);
+        SAXReader reader = new SAXReader();
+        //读取xml文件到Document中
+        Document doc = reader.read(file);
+        //获取xml文件的根节点
+        Element rootElement = doc.getRootElement();
+        //读取context借点
+        Element contextElement = rootElement.element("context");
+        //定义一个Element用于遍历
+        Element tableElement;
+        //取第一个"table"节点
+        tableElement=contextElement.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName");
+        String tableName= tableElement.attributeValue("tableName");
+        String tableNameCn = DbUtil.getTableComment(tableName);
+        String domain=Domain.substring(0,1).toUpperCase().substring(1);
+        System.out.println("表:"+tableElement.attributeValue("tableName"));
+        System.out.println("Domian:"+tableElement.attributeValue("domainObjectName"));
         String module = MODULE;
-        List<Field> fieldList = DbUtil.getColumnByTableName(domain);
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName);
         Set<String> typeSet = getJavaTypes(fieldList);
         Map<String, Object> map = new HashMap<>();
         map.put("Domain", Domain);
         map.put("domain", domain);
         map.put("tableNameCn", tableNameCn);
         map.put("module", module);
-        map.put("fieldList",fieldList);
+        map.put("fieldList", fieldList);
         map.put("typeSet", typeSet);
         FreemarkUtil.initConfig("service.ftl");
         FreemarkUtil.generator(toServicePath + Domain + "Service.java", map);
@@ -42,13 +65,14 @@ public class ServerGenerator {
 
     /**
      * 获取所有JAVA类型 使用Set去重
+     *
      * @param fieldList
      * @return
      */
     private static Set<String> getJavaTypes(List<Field> fieldList) {
         Set<String> set = new HashSet<>();
-        for (int i = 0; i<fieldList.size();i++){
-            Field field=fieldList.get(i);
+        for (int i = 0; i < fieldList.size(); i++) {
+            Field field = fieldList.get(i);
             set.add(field.getJavaType());
         }
         return set;
