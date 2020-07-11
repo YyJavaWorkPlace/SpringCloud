f7a33d166207e96f9cf99112ce2fa30224bf685d
YyJavaWorkPlace <13383448815@163.com>
2020-07-11 22:16:57 +0800

    1.增加时长格式化过滤器formatSecond

--- admin/src/filter/filter.js
+++ admin/src/filter/filter.js
@@ -6,18 +6,18 @@
  * 首先判断参数是否为空
  * 传入数组 和 key 最后返回
  */
-let optionKV=(object,key)=>{
-  if (!object||!key){
+let optionKV = (object, key) => {
+    if (!object || !key) {
         return "";
-  } else{
-      let result="";
-      for (let enums in object){
-          if (key === object[enums]["key"]){
-              result = object[enums]["value"];
-          }
-      }
-      return result;
-  }
+    } else {
+        let result = "";
+        for (let enums in object) {
+            if (key === object[enums]["key"]) {
+                result = object[enums]["value"];
+            }
+        }
+        return result;
+    }
 };
 
 /**
@@ -28,20 +28,49 @@ let optionKV=(object,key)=>{
  * 首先判断参数是否为空
  * 传入数组 和 key 最后返回
  */
-let optionArray=(list,key)=>{
-    if (!list||!key){
+let optionArray = (list, key) => {
+    if (!list || !key) {
         return "";
-    } else{
-        let result="";
-        for (let i = 0;i<list.length;i++){
-            if (key === list[i]["key"]){
+    } else {
+        let result = "";
+        for (let i = 0; i < list.length; i++) {
+            if (key === list[i]["key"]) {
                 result = list[i]["value"];
             }
         }
         return result;
     }
 };
+let formatSecond = (value) => {
+    value = value || 0;
+    let second = parseInt(value, 10); //十进制解析字符串 返回整数
+    let minute = 0;//分
+    let hour = 0;//小时
+    if (second > 60) {
+        minute = Math.floor(second / 60);
+        second = Math.floor(second % 60);
+        if (minute > 60) {
+            hour = Math.floor(minute / 60);
+            minute = Math.floor(minute % 60)
+        }
+    } else {
+        //直接显示秒数
+    }
+    let result = "" + PrefixInteger(second, 2) + "";
+    result = "" + PrefixInteger(minute, 2) + ":" + result;
+    result = "" + PrefixInteger(hour, 2) + ":"+result;
+    return result;
+};
+/**
+ * 补0操作
+ * @return {string}
+ */
+var PrefixInteger = function (num, length) {
+    return (Array(length).join('0') + num).slice(-length);
+};
+
 export default {
     optionKV,
-    optionArray
+    optionArray,
+    formatSecond
 }
