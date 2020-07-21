b396007628c184831063c85b072e9a1e0e10eba3
YyJavaWorkPlace <13383448815@163.com>
2020-07-21 23:23:41 +0800

    1.文件上传功能开发 springboot静态资源配置 配置文件访问路径

---
+++ file/src/main/java/com/course/file/config/SpringMvcConfig.java
@@ -0,0 +1,20 @@
+package com.course.file.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author yk
+ * @version 1.0
+ * @date 2020/7/21 23:14
+ * @desc SpringBoot静态资源配置 静态资源包含图片 CSS JS
+ * /f 是一个请求标识 静态资源
+ */
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/f/**").addResourceLocations("file:F:/uploadVideo/");
+    }
+}
