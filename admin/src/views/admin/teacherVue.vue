c11f2f2a67d386acc9657a9515134d80e83a3e3c
YyJavaWorkPlace <13383448815@163.com>
2020-07-23 22:58:06 +0800

    1.文件上传功能开发 增加上传文件类型判断

--- admin/src/views/admin/teacherVue.vue
+++ admin/src/views/admin/teacherVue.vue
@@ -120,7 +120,8 @@
                                     <i class="ace-icon fa fa-upload"></i>
                                     上传头像
                                 </button>
-                                <input class="hidden" id="file-upload-input" type="file" v-on:change="uploadImage()">
+                                <input class="hidden" ref="file" id="file-upload-input" type="file"
+                                       v-on:change="uploadImage()">
                                 <div v-show="teacher.image" class="row">
                                     <div class="col-md-4">
                                         <img v-bind:src="teacher.image" class="img-responsive">
@@ -245,7 +246,25 @@
                 let _this = this;
                 let formData = new window.FormData();
                 // key :"file"必须和后端controller参数名一致
-                formData.append("file", document.querySelector('#file-upload-input').files[0]);
+                let file = _this.$refs.file.files[0];
+                //判断文件上传格式
+                let suffixs = ["jpg", "jpeg", "png"];
+                let fileName = file.name;
+                //获取上传文件的文件名后缀
+                let suffix = fileName.substring(fileName.lastIndexOf(".") + 1, File.length).toLocaleLowerCase();
+                let validateSuffix = false;
+                for (let i = 0; i < suffixs.length; i++) {
+                    if (suffixs[i].toLocaleLowerCase() == suffix) {
+                        validateSuffix = true;
+                        break;
+                    }
+                }
+                //校验未通过
+                if (!validateSuffix) {
+                    Toast.warning("文件格式不正确，仅支持上传" + suffixs.join(","));
+                    return;
+                }
+                formData.append("file", file);
                 Loading.show();
                 _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/upload', formData).then((response) => {
                     Loading.hide();
