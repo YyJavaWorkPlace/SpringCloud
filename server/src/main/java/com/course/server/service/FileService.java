7cd7e95dc31f663ba905dd339f4aace7c8e39813
YyJavaWorkPlace <13383448815@163.com>
2020-08-03 23:43:35 +0800

    1.大文件断点续传与急速妙传：文件表增加分片相关字段

--- server/src/main/java/com/course/server/service/FileService.java
+++ server/src/main/java/com/course/server/service/FileService.java
@@ -1,5 +1,4 @@
 package com.course.server.service;
-
 import com.course.server.domain.File;
 import com.course.server.domain.FileExample;
 import com.course.server.dto.FileDto;
@@ -14,8 +13,7 @@ import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.Date;
-
+        import java.util.Date;
 /**
  * File 大章表业务逻辑
  */
@@ -42,7 +40,7 @@ public class FileService {
     }
 
     public void save(FileDto fileDto) {
-        File file = CopyUtil.copy(fileDto, File.class);
+        File file = CopyUtil.copy(fileDto,File.class);
         if (StringUtils.isEmpty(fileDto.getId())) {
             this.insert(file);
         } else {
@@ -51,14 +49,14 @@ public class FileService {
     }
 
     private void insert(File file) {
-        Date now = new Date();
-        file.setCreatedAt(now);
+            Date now = new Date();
+                file.setUpdatedAt(now);
         file.setId(UuidUtil.getShortUuid());
         fileMapper.insert(file);
     }
 
     private void update(File file) {
-        file.setUpdatedAt(new Date());
+              file.setUpdatedAt(new Date());
         fileMapper.updateByPrimaryKey(file);
     }
 
